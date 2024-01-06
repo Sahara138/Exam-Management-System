e0420a0fb211eb182a954218aa79c32a76f07bba
sahara-islam <saharaiislam586@gmail.com>
2024-01-07 02:55:46 +0600

    add files

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+<footer>
+    <span>&copy; Code Red<br>Asif Hasan Abir<br>Najim Ullah Rifat<br>Sahara Islam<br>Department of Computer Science<br>Southeast University</span>
+</footer>
+
+<script src="<?php echo _get_base_url(); ?>jquery.js"></script>
+<script src="<?php echo _get_base_url(); ?>script.js"></script>
+
+</body>
+</html>
